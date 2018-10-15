a86268f5db2fc0b5c5bfe153b2e8ef9e0985ac2b
Administrator <ssunil.tikoo@gmail.com>
2018-10-15 17:49:05 +0530

    git temp

---
+++ src/com/sunil/app/main.java
@@ -0,0 +1,13 @@
+package com.sunil.app;
+
+import com.sunil.logic.Calculator;
+
+public class main {
+
+    public static void main(String[] args) {
+        Calculator c1 = new Calculator();
+        double k = c1.doSum(12.2,5656.22);
+        System.out.println(k);
+    }
+
+}
---
+++ src/com/sunil/logic/CalcUI.java
@@ -0,0 +1,9 @@
+package com.sunil.logic;
+
+public class CalcUI {
+     static void main(String[] args) {
+        Calculator c = new Calculator();
+        c.doSum(12.9,99.52);
+
+    }
+}
