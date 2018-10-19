8b8989118a5bc5d398de0b573de415d5d8985e33
Administrator <ssunil.tikoo@gmail.com>
2018-10-19 11:29:27 +0530

    changes updated

---
+++ src/com/sunil/BankApp/Account.java
@@ -0,0 +1,48 @@
+package com.sunil.BankApp;
+
+public abstract class Account {
+    private int accountNo;
+    private String customerName;
+    private double balance;
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+
+
+    public int getAccountNo() {
+        return accountNo;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public Account(int accountNo,String customerName,double balance){
+        this.accountNo = accountNo;
+        this.customerName = customerName;
+        this.balance = balance;
+    }
+
+    public void Deposit(double amount) {
+        double bal = getBalance() + amount;
+        System.out.println("Amount deposited.balance " + bal);
+
+    }
+
+    public void Withdraw(double amount) {
+        double bal = getBalance()  - amount;
+        System.out.println("Amount withdraw. bal" + bal);
+    }
+
+
+    public void getAccountInfo(){
+        System.out.println("Account statement"+getAccountNo()+getBalance()+getCustomerName());
+    }
+
+}
--- src/com/sunil/DemoComputerP/Hardware/BuildComputer.java
+++ src/com/sunil/DemoComputerP/Hardware/BuildComputer.java
@@ -4,6 +4,8 @@ import com.sunil.DemoComputerP.Computer;
 
 public class BuildComputer {
     public static void main(String[] args) {
+
+        // variable of Base Type can hold object of derived type - Polymorphic object creation
         Storage hd = new HardDisk();
         Display mon = new Monitor();
         Computer c = new Computer(hd,mon);
